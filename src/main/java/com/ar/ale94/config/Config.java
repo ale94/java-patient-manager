f7cdf13dab176cfc7230140ffa6e01f98b4bbca5
ale94 <alejandrorua111@gmail.com>
2025-04-21 23:13:45 -0300

    feat: add database config

--- src/main/java/com/ar/ale94/config/Config.java
+++
@@ -1,4 +0,0 @@
-package com.ar.ale94.config;
-
-public class Config {
-}
---
+++ src/main/java/com/ar/ale94/config/DatabaseConfig.java
@@ -0,0 +1,7 @@
+package com.ar.ale94.config;
+
+public class DatabaseConfig {
+    public static final String URL = "jdbc:mysql://localhost:3306/db_gestor_medica";
+    public static final String USER = "root";
+    public static final String PASSWORD = "alejandro12";
+}
